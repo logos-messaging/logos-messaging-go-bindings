66722e0a356b8537326f3c0857b7c62692214325
Ivan Folgueira Bande <ivansete@status.im>
2026-01-25 20:35:45 +0100

    chore: update message_hash to latest go-waku implementation

--- waku/common/message_hash.go
+++ waku/common/message_hash.go
@@ -1,44 +1,75 @@
 package common
 
 import (
-	"encoding/hex"
-	"errors"
-	"fmt"
+	"crypto/sha256"
+	"encoding/binary"
+	"hash"
+	"sync"
+
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+
+	"github.com/logos-messaging/logos-messaging-go-bindings/waku/pb"
 )
 
 // MessageHash represents an unique identifier for a message within a pubsub topic
-type MessageHash string
-
-func ToMessageHash(val string) (MessageHash, error) {
-	if len(val) == 0 {
-		return "", errors.New("empty string not allowed")
-	}
-
-	if len(val) < 2 || val[:2] != "0x" {
-		return "", errors.New("string must start with 0x")
-	}
-
-	// Remove "0x" prefix for hex decoding
-	hexStr := val[2:]
-
-	// Verify the remaining string is valid hex
-	_, err := hex.DecodeString(hexStr)
-	if err != nil {
-		return "", fmt.Errorf("invalid hex string: %v", err)
-	}
-
-	return MessageHash(val), nil
-}
+type MessageHash [32]byte
 
 func (h MessageHash) String() string {
-	return string(h)
+	return hexutil.Encode(h[:])
 }
 
-func (h MessageHash) Bytes() ([]byte, error) {
-	s := string(h)
-	// Remove 0x prefix if present
-	if len(s) >= 2 && s[:2] == "0x" {
-		s = s[2:]
+func (h MessageHash) Bytes() []byte {
+	return h[:]
+}
+
+// ToMessageHash converts a byte slice into a MessageHash
+func ToMessageHash(b []byte) MessageHash {
+	var result MessageHash
+	copy(result[:], b)
+	return result
+}
+
+var sha256Pool = sync.Pool{New: func() interface{} {
+	return sha256.New()
+}}
+
+// SHA256 generates the SHA256 hash from the input data
+func SHA256(data ...[]byte) []byte {
+	h, ok := sha256Pool.Get().(hash.Hash)
+	if !ok {
+		h = sha256.New()
 	}
-	return hex.DecodeString(s)
+	defer sha256Pool.Put(h)
+	h.Reset()
+	for i := range data {
+		h.Write(data[i])
+	}
+	return h.Sum(nil)
+}
+
+// Hash calculates the hash of a waku message
+func (msg *pb.WakuMessage) Hash(pubsubTopic string) MessageHash {
+	hash := SHA256([]byte(pubsubTopic), msg.Payload, []byte(msg.ContentTopic), msg.Meta, toBytes(msg.GetTimestamp()))
+	return ToMessageHash(hash)
+}
+
+func toBytes(i int64) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, uint64(i))
+	return b
+}
+
+func (msg *pb.WakuMessage) LogFields(pubsubTopic string) []zapcore.Field {
+	return []zapcore.Field{
+		zap.Stringer("hash", msg.Hash(pubsubTopic)),
+		zap.String("pubsubTopic", pubsubTopic),
+		zap.String("contentTopic", msg.ContentTopic),
+		zap.Int64("timestamp", msg.GetTimestamp()),
+	}
+}
+
+func (msg *pb.WakuMessage) Logger(logger *zap.Logger, pubsubTopic string) *zap.Logger {
+	return logger.With(msg.LogFields(pubsubTopic)...)
 }
